26562fbe9b17fa51c2b73e72e894e4546f554d37
lewis <ljones140@gmail.com>
2015-12-14 11:30:19 +0000

    calculates multiple journeys correctly

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,21 +1,30 @@
 class Journey < ActiveRecord::Base
 
-  START_LAT = 51.519812
-  START_LON = -0.093918626
+  START_LOCATION = "51.51981%2C-0.093918626"
 
   has_many :visits
 
   def calculate_arrival_times
-    visits.each do |visit|
-      visit.arrival_time = start_time + travel_minutes(visit)*60
-      visit.save
+    visits.each_with_index do |visit, index|
+      if index == 0
+        visit.arrival_time = start_time + travel_minutes(visit, START_LOCATION, start_time)*60
+        visit.save
+      else
+        start_location = "#{visits[index -1].latitude}%2C#{visits[index -1].longitude}"
+        visit.arrival_time = visits[index -1].arrival_time + travel_minutes(visit, start_location, visits[index -1].arrival_time)*60
+      end
     end
   end
 
   private
 
-  def travel_minutes(visit)
-    response = HTTParty.get("https://developer.citymapper.com/api/1/traveltime/?startcoord=#{START_LAT}%2C#{START_LON}&endcoord=#{visit.latitude}%2C#{visit.longitude}&time=2015-12-13T19%3A00%3A02-0500&time_type=departure&key=#{CITY_MAPPER_KEY}")
+  def travel_minutes(visit, start_location, time)
+    request = "https://developer.citymapper.com/api/1/traveltime/?startcoord=#{start_location}&endcoord=#{visit.latitude}%2C#{visit.longitude}&time=#{time}&time_type=departure&key=#{CITY_MAPPER_KEY}"
+    city_mapper_call(request)
+  end
+
+  def city_mapper_call(request)
+    response = HTTParty.get(request)
     response["travel_time_minutes"]
   end
 
--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -6,16 +6,22 @@ RSpec.describe Journey, type: :model do
 
   context 'when has a visit' do
 
-    let(:journey) { create(:journey, :with_visit) }
-    let(:visit) { journey.visits.first }
+    let(:journey) { create(:journey, :with_two_visits) }
+    let(:first_visit) { journey.visits.first }
+    let(:second_visit) { journey.visits.last }
 
     before do
-      allow(journey).to receive(:travel_minutes) { 10 }
+      allow(journey).to receive(:city_mapper_call) { 10 }
     end
 
-    it 'calculates arrival time' do
+    it 'calculates arrival time of first visit' do
       journey.calculate_arrival_times
-      expect(visit.arrival_time).to eq("2015-12-13 18:10:00.000000000 +0000")
+      expect(first_visit.arrival_time).to eq("2015-12-13 18:10:00.000000000 +0000")
+    end
+
+    it 'caclulates arrival time of second visit' do
+      journey.calculate_arrival_times
+      expect(second_visit.arrival_time).to eq("2015-12-13 18:20:00.000000000 +0000")
     end
 
   end
